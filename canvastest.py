cc8e595230bf2fce12e0f8cbb15f9e434ce53a32
lockiie <lachlan.mcewen@education.nsw.gov.au>
2016-07-09 23:25:48 +1000

    Hardcoded Images
    Hardcoded the images

--- canvastest.py
+++ canvastest.py
@@ -1,27 +1,10 @@
-import tkinter
+import tkinter as tk
 
+root=tk.Tk()
+f1 = tk.Frame(width=200, height=200, background="red")
+f2 = tk.Frame(width=100, height=100, background="blue")
 
-def findXCenter(canvas, item):
-    coords = canvas.bbox(item)
-    xOffset = (1280 / 2) - ((coords[2] - coords[0]) / 2)
-    return xOffset
+f1.pack(fill="both", expand=True, padx=20, pady=20)
+f2.place(in_=f1, anchor="c", relx=.5, rely=.5)
 
-master = tkinter.Tk()
-
-#width, height = Image.open(image.png).size
-
-canvas = tkinter.Canvas(master, width=1280, height=720)
-canvas.pack()
-
-background_image = tkinter.PhotoImage(file = 'D:\\School\\Year 11\\Software Design and Development\\SimpleilyShapeily\\Simpleily-Shapeily\\resources\\background.gif')
-canvas.create_image(0, 0, image=background_image, anchor="nw")
-
-canvas_id = canvas.create_text(10, 10, anchor="nw")
-canvas.itemconfig(canvas_id, text="Simpily Shapeily", width=1280)
-canvas.itemconfig(canvas_id, font=("verdana", 25))
-xOffset = findXCenter(canvas, canvas_id)
-canvas.move(canvas_id, xOffset, 0)
-
-#canvas.create_text(2, 2, text="Python")
-
-tkinter.mainloop()
+root.mainloop()
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ window.resizable(0,0)
 ent = tkinter.Entry(window)
 
 frameOne = tkinter.Frame(window)
+frameTwo = tkinter.Frame(window)
 
 canvasTitle = tkinter.Canvas(frameOne, width=1280, height=720)
 canvasTitle.pack()
@@ -28,6 +29,17 @@ canvasTitle.itemconfig(canvas_id, font=("verdana", 25))
 xOffset = findXCenter(canvasTitle, canvas_id)
 canvasTitle.move(canvas_id, xOffset, 75)
 
+childSam = tkinter.PhotoImage(file = 'D:\\School\\Year 11\\Software Design and Development\\SimpleilyShapeily\\Simpleily-Shapeily\\resources\\childSam.gif')
+childLamb = tkinter.PhotoImage(file = 'D:\\School\\Year 11\\Software Design and Development\\SimpleilyShapeily\\Simpleily-Shapeily\\resources\\childLamb.gif')
+childDamn = tkinter.PhotoImage(file = 'D:\\School\\Year 11\\Software Design and Development\\SimpleilyShapeily\\Simpleily-Shapeily\\resources\\childDamn.gif')
+childGram = tkinter.PhotoImage(file = 'D:\\School\\Year 11\\Software Design and Development\\SimpleilyShapeily\\Simpleily-Shapeily\\resources\\childGram.gif')
+
+image_childSam = tkinter.Label(frameTwo, image=childSam).grid(column = 0, row = 0)
+image_labelchildLamb = tkinter.Label(frameTwo, image=childLamb).grid(column = 1, row = 0)
+image_labelchildDamn = tkinter.Label(frameTwo, image=childDamn).grid(column = 2, row = 0)
+image_labelchildGram = tkinter.Label(frameTwo, image=childGram).grid(column = 3, row = 0)
+
 frameOne.pack()
+frameTwo.place(in_=frameOne)
 window.mainloop()
 
