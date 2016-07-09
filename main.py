bb48fa9c7382150ea6649388ebc26b14b0668da6
lockiie <lachlan.mcewen@education.nsw.gov.au>
2016-07-09 22:19:51 +1000

    Working GUI using canvases

--- main.py
+++ main.py
@@ -9,20 +9,25 @@ def findXCenter(canvas, item):
 window = tkinter.Tk()
 window.title("Simplily Shapeily")
 window.geometry("1280x720")
+window.resizable(0,0)
 ent = tkinter.Entry(window)
 
-canvas = tkinter.Canvas(window, width=1280, height=720)
-canvas.pack()
+frameOne = tkinter.Frame(window)
+
+canvasTitle = tkinter.Canvas(frameOne, width=1280, height=720)
+canvasTitle.pack()
+
+
 
 background_image = tkinter.PhotoImage(file = 'D:\\School\\Year 11\\Software Design and Development\\SimpleilyShapeily\\Simpleily-Shapeily\\resources\\background.gif')
-canvas.create_image(0, 0, image=background_image, anchor="nw")
+canvasTitle.create_image(0, 0, image=background_image, anchor="nw")
 
-canvas_id = canvas.create_text(10, 10, anchor="nw")
-canvas.itemconfig(canvas_id, text="Simpily Shapeily", width=1280)
-canvas.itemconfig(canvas_id, font=("verdana", 25))
-xOffset = findXCenter(canvas, canvas_id)
-canvas.move(canvas_id, xOffset, 0)
+canvas_id = canvasTitle.create_text(10, 10, anchor="nw")
+canvasTitle.itemconfig(canvas_id, text="Simpily Shapeily", width=1280)
+canvasTitle.itemconfig(canvas_id, font=("verdana", 25))
+xOffset = findXCenter(canvasTitle, canvas_id)
+canvasTitle.move(canvas_id, xOffset, 75)
 
-#Sets up the mainloop and frames. Buttons are within frameone and the screen is frametwo.
+frameOne.pack()
 window.mainloop()
 
