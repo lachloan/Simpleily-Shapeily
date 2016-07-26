e2697e8708ed41156917d238eb1593d181dc172d
lockiie <lachlan.mcewen@education.nsw.gov.au>
2016-07-26 21:31:36 +1000

    Finished Basic Game
    Most of the game is finished. Added lines connecting shapes, added a
    toplevel window to display the "win" and reset the game to be played
    again

--- actualtesting.py
+++ actualtesting.py
@@ -17,32 +17,25 @@ def run():
 
     canvas.pack()
 
-    window.bind("<ButtonPress-1>", clickHold)
-    window.bind("<ButtonRelease-1>", clickRelease)
+    shapes = []
+    canvases = []
 
-    start_x = None
-    start_y = None
+    canvases.append(tkinter.Canvas(frameTesting, width=720, height=720))
 
+    canvases[0].pack()
+
+    shapes.append(canvases[0].create_rectangle(0,0,50,50, fill="black"))
+    shapes.append(canvases[0].create_oval(50,50,100,100, fill="black"))
+
+
+
+
+
+    print(shapes)
+    print(canvases)
     frameTesting.pack()
     window.mainloop()
 
-def clickHold(event):
-    global start_y, start_x
-
-    print("Running click")
-    start_x = event.x
-    start_y = event.y
-
-    point_start = canvas.create_rectangle(1, 100, 2, 90, fill="black")
-
-def clickRelease(event):
-    global start_y, start_x
-
-    end_x = event.x
-    end_y = event.y
-
-    line = canvas.create_line(start_x, start_y, end_x, end_y,fill="black")
-    point_end = canvas.create_rectangle(end_x - 10, end_y - 10, end_x + 10, end_y + 10, fill="black")
 
 
 run()
--- game.py
+++ game.py
@@ -2,8 +2,11 @@ from tkinter import ttk
 import tkinter
 import database
 import random
+import time
 
 def run():
+    global clickedcurrentlyNames, clickedcurrentlyShapes, shape_pairs, count_pairs, shapeNameCoords, calledNames, calledShapes, count_pairs
+
     global_background = "#C1CDCD"
     shape_colour = "#808098"
     text_colour = "#3A4161"
@@ -23,121 +26,206 @@ def run():
     frameShapes = tkinter.Frame(window, bg=global_background)
     frameNames = tkinter.Frame(window, bg=global_background)
 
-    shapes_called = []
+    calledShapes = []
+
+    clickedcurrentlyShapes = 0
+    clickedcurrentlyNames = 0
+
+    shape_pairs = {}
+    count_pairs = 0
+    shapeNameCoords = {}
+
+    calledNames = None
+
+    def check_shapes():
+        global clickedcurrentlyShapes, clickedcurrentlyNames, shape_pairs, count_pairs, calledNames, calledShapes
+
+
+        if clickedcurrentlyShapes and clickedcurrentlyNames is not 0:
+            shape_pairs[clickedcurrentlyNames] = clickedcurrentlyShapes
+            print(shape_pairs)
+
+            Coord_X = [270, 640, 1010]
+            Coord_YS = 383
+            Coord_YN = 180
+
+            Pos_Shape = calledShapes.index(clickedcurrentlyShapes)
+            Pos_Name = calledNames.index(clickedcurrentlyNames)
+
+            if Pos_Name == Pos_Shape:
+                line_canvas.create_line(Coord_X[Pos_Name], Coord_YN, Coord_X[Pos_Name], Coord_YS)
+
+            elif Pos_Name < Pos_Shape:
+                line_canvas.create_line(Coord_X[Pos_Name], Coord_YN, Coord_X[Pos_Name], Coord_YN + 40) # Down
+                line_canvas.create_line(Coord_X[Pos_Name], Coord_YN + 40, Coord_X[Pos_Name] + 150, Coord_YN + 40) # Across
+                line_canvas.create_line(Coord_X[Pos_Name] + 150, Coord_YN + 40, Coord_X[Pos_Name] + 150, Coord_YS - 40) # Down
+                line_canvas.create_line(Coord_X[Pos_Name] + 150, Coord_YS - 40, Coord_X[Pos_Shape], Coord_YS - 40)  # Across
+                line_canvas.create_line(Coord_X[Pos_Shape], Coord_YS - 40, Coord_X[Pos_Shape],Coord_YS)  # Down
+
+            elif Pos_Name > Pos_Shape:
+                line_canvas.create_line(Coord_X[Pos_Name], Coord_YN, Coord_X[Pos_Name], Coord_YN + 70)  # Down
+                line_canvas.create_line(Coord_X[Pos_Name], Coord_YN + 70, Coord_X[Pos_Name] - 150, Coord_YN + 70)  # Across
+                line_canvas.create_line(Coord_X[Pos_Name] - 150, Coord_YN + 70, Coord_X[Pos_Name] - 150, Coord_YS - 70)  # Down
+                line_canvas.create_line(Coord_X[Pos_Name] - 150, Coord_YS - 70, Coord_X[Pos_Shape], Coord_YS - 70)  # Across
+                line_canvas.create_line(Coord_X[Pos_Shape], Coord_YS - 70, Coord_X[Pos_Shape], Coord_YS)  # Down
+
+
+            clickedcurrentlyShapes = 0
+            clickedcurrentlyNames = 0
+            count_pairs = count_pairs + 1
+
+        if count_pairs == 3:
+            matches = 0
+            for key, value in shape_pairs.items():
+                if key == value:
+                    matches = matches + 1
+
+            if matches == 3:
+                print("All Correct")
+                win()
 
     def generateShapes(x):
-        global shapes_called
+        global calledShapes
         global shape_canvas_square, shape_canvas_triangle, shape_canvas_circle, shape_canvas_diamond
 
         shapes_list = ['square', 'triangle', 'circle', 'diamond']
         count = 0
-        shapes_called = []
+        calledShapes = []
         shape_padx = 100
+        shapes_shapes = []
 
         while count < 3: # How many shapes to display
             shape = random.choice(shapes_list)
-            while shape in shapes_called: # Get unique shape (gets rid of duplicates)
+            while shape in calledShapes: # Get unique shape (gets rid of duplicates)
                 shape = random.choice(shapes_list)
 
             if shape == "square":
                 shape_canvas_square = tkinter.Canvas(frameShapes, width=170, height=170, bg=global_background, bd=0,
                                                      highlightthickness=0, relief="ridge")
-                shape_square = shape_canvas_square.create_rectangle(10, 10, 160, 160, fill=shape_colour,
-                                                                outline=global_background)  # Square
+                shapes_shapes.append(shape_canvas_square.create_rectangle(10, 10, 160, 160, fill=shape_colour,
+                                                                outline=global_background))  # Square
                 shape_canvas_square.grid(row=1, column=count, padx=shape_padx)
                 shape_canvas_square.bind("<ButtonPress-1>", on_click_square)
 
             elif shape == "triangle":
                 shape_canvas_triangle = tkinter.Canvas(frameShapes, width=170, height=170, bg=global_background, bd=0,
                                                        highlightthickness=0, relief="ridge")
-                shape_triangle = shape_canvas_triangle.create_polygon(10, 160, 85, 10, 160, 160, fill=shape_colour,
-                                                                outline=global_background)  # Triangle
+                shapes_shapes.append(shape_canvas_triangle.create_polygon(10, 160, 85, 10, 160, 160, fill=shape_colour,
+                                                                outline=global_background))
                 shape_canvas_triangle.grid(row=1, column=count, padx=shape_padx)
                 shape_canvas_triangle.bind("<ButtonPress-1>", on_click_triangle)
 
             elif shape == "circle":
                 shape_canvas_circle = tkinter.Canvas(frameShapes, width=170, height=170, bg=global_background, bd=0,
                                                      highlightthickness=0, relief="ridge")
-                shape_circle = shape_canvas_circle.create_oval(10, 10, 160, 160, fill=shape_colour,
-                                                                outline=global_background)  # Circle
+                shapes_shapes.append(shape_canvas_circle.create_oval(10, 10, 160, 160, fill=shape_colour,
+                                                                outline=global_background))
                 shape_canvas_circle.grid(row=1, column=count, padx=shape_padx)
                 shape_canvas_circle.bind("<ButtonPress-1>", on_click_circle)
 
             elif shape == "diamond":
                 shape_canvas_diamond = tkinter.Canvas(frameShapes, width=170, height=170, bg=global_background, bd=0,
                                                       highlightthickness=0, relief="ridge")
-                shape_diamond = shape_canvas_diamond.create_polygon(10, 85, 85, 10, 160, 85, 85, 160, fill=shape_colour,
-                                                                outline=global_background)  # Diamond
+                shapes_shapes.append(shape_canvas_diamond.create_polygon(10, 85, 85, 10, 160, 85, 85, 160, fill=shape_colour,
+                                                                outline=global_background))  # Diamond
                 shape_canvas_diamond.grid(row=1, column=count, padx=shape_padx)
                 shape_canvas_diamond.bind("<ButtonPress-1>", on_click_diamond)
 
-            shapes_called.append(shape)
-            print(shapes_called)
+            calledShapes.append(shape)
 
             count += 1
 
-        print(shapes_called)
         generateNames()
 
+    #
+    # # Clicking Section
+    #
+
+    # Square Clicks
+
     def on_click_square_name(event):
-        global name_canvas_square
-        print("clicked square")
-        name_square_box = name_canvas_square.create_rectangle(0,0,119,74, fill="", outline=border_colour)
-        name_square_box = name_canvas_square.create_rectangle(55, 65, 65, 75, fill=border_colour, outline=border_colour)
+        global name_canvas_square, clickedcurrentlyNames
 
+        if clickedcurrentlyNames == 0:
+            clickedcurrentlyNames = "square"
+            name_square_box = name_canvas_square.create_rectangle(0,0,119,74, fill="", outline=border_colour)
+            name_square_box = name_canvas_square.create_rectangle(55, 65, 65, 75, fill=border_colour, outline=border_colour)
     def on_click_square(event):
-        global shape_canvas_square
-        shape_canvas_square.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+        global shape_canvas_square, clickedcurrentlyShapes
 
+        if clickedcurrentlyShapes == 0:
+            clickedcurrentlyShapes = "square"
+            shape_canvas_square.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+            check_shapes()
+
+    # Circle Clicks
     def on_click_circle_name(event):
-        global name_canvas_circle
-        print("clicked circle")
-        name_circle_box = name_canvas_circle.create_rectangle(0,0,119,74, fill="", outline=border_colour)
-        name_circle_box = name_canvas_circle.create_rectangle(55,65,65,75, fill=border_colour, outline=border_colour)
+        global name_canvas_circle, clickedcurrentlyNames
 
-
+        if clickedcurrentlyNames == 0:
+            clickedcurrentlyNames = "circle"
+            name_circle_box = name_canvas_circle.create_rectangle(0,0,119,74, fill="", outline=border_colour)
+            name_circle_box = name_canvas_circle.create_rectangle(55,65,65,75, fill=border_colour, outline=border_colour)
     def on_click_circle(event):
-        global shape_canvas_circle
-        shape_canvas_circle.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+        global shape_canvas_circle, clickedcurrentlyShapes
 
+        if clickedcurrentlyShapes == 0:
+            clickedcurrentlyShapes = "circle"
+            shape_canvas_circle.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+            check_shapes()
+
+    # Triangle Clicks
     def on_click_triangle_name(event):
-        global name_canvas_triangle
-        print("clicked triangle")
-        name_triangle_box = name_canvas_triangle.create_rectangle(0,0,119,74, fill="", outline=border_colour)
-        name_triangle_box = name_canvas_triangle.create_rectangle(55, 65, 65, 75, fill=border_colour, outline=border_colour)
+        global name_canvas_triangle, clickedcurrentlyNames
 
+        if clickedcurrentlyNames == 0:
+            clickedcurrentlyNames = "triangle"
+            name_triangle_box = name_canvas_triangle.create_rectangle(0,0,119,74, fill="", outline=border_colour)
+            name_triangle_box = name_canvas_triangle.create_rectangle(55, 65, 65, 75, fill=border_colour, outline=border_colour)
     def on_click_triangle(event):
-        global shape_canvas_triangle
-        shape_canvas_triangle.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+        global shape_canvas_triangle, clickedcurrentlyShapes
 
+        if clickedcurrentlyShapes == 0:
+            clickedcurrentlyShapes = "triangle"
+            shape_canvas_triangle.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+            check_shapes()
+
+    # Diamond Clicks
     def on_click_diamond_name(event):
-        global name_canvas_diamond
-        print("clicked diamond")
-        name_diamond_box = name_canvas_diamond.create_rectangle(0,0,119,74, fill="", outline=border_colour)
-        name_diamond_box = name_canvas_diamond.create_rectangle(55, 65, 65, 75, fill=border_colour,
+        global name_canvas_diamond, clickedcurrentlyNames
+
+        if clickedcurrentlyNames == 0:
+            clickedcurrentlyNames = "diamond"
+            name_diamond_box = name_canvas_diamond.create_rectangle(0,0,119,74, fill="", outline=border_colour)
+            name_diamond_box = name_canvas_diamond.create_rectangle(55, 65, 65, 75, fill=border_colour,
                                                                   outline=border_colour)
-
     def on_click_diamond(event):
-        global shape_canvas_diamond
-        shape_canvas_diamond.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+        global shape_canvas_diamond, clickedcurrentlyShapes
 
+        if clickedcurrentlyShapes == 0:
+            clickedcurrentlyShapes = "diamond"
+            shape_canvas_diamond.create_rectangle(0,0,169,169, fill="", outline=border_colour)
+            check_shapes()
 
+    #
+    # # End Clicking Section
+    #
 
-
+    # Generation of names for shapes
     def generateNames():
-        global shapes_called, name_canvas_square, name_canvas_circle, name_canvas_diamond, name_canvas_triangle
-        name_called = []
+        global calledShapes, name_canvas_square, name_canvas_circle, name_canvas_diamond, name_canvas_triangle, calledNames
+        calledNames = []
         count = 0
         print("Generating Names")
-        padx = 127
+        padx = 125
 
-        for i in shapes_called:
+        for i in calledShapes:
             count = count + 1
-            name = random.choice(shapes_called)
-            while name in name_called:
-                name = random.choice(shapes_called)
+            name = random.choice(calledShapes)
+            while name in calledNames:
+                name = random.choice(calledShapes)
 
-            name_called.append(name)
+            calledNames.append(name)
             if name == "square":
                 name_canvas_square = tkinter.Canvas(frameNames, width=120, height=75, bg=global_background, bd=0,
                                                     highlightthickness=0, relief="ridge")
@@ -145,6 +233,7 @@ def run():
                 name_square = name_canvas_square.create_text(60,37.5, text="Square")
                 name_canvas_square.itemconfig(name_square, font=("MyriadPro-Regular", 20,), fill=text_colour)
                 name_canvas_square.bind("<ButtonPress-1>", on_click_square_name)
+
 
             elif name == "circle":
                 name_canvas_circle = tkinter.Canvas(frameNames, width=120, height=75, bg=global_background, bd=0,
@@ -170,19 +259,24 @@ def run():
                 name_canvas_diamond.itemconfig(name_diamond, font=("MyriadPro-Regular", 20), fill=text_colour)
                 name_canvas_diamond.bind("<ButtonPress-1>", on_click_diamond_name)
 
-            print(name_called)
-            print(count)
+    def win():
+        global toplevel
+        toplevel = tkinter.Toplevel()
+        winner = tkinter.Label(toplevel, text="You won!")
+        continuebutton = tkinter.Button(toplevel, text="Continue", command=toplevel.destroy)
+        reset()
+        winner.pack(pady = 20, padx = 10)
+        continuebutton.pack(pady = 5)
+
+    def reset():
+        global count_pairs, shape_pairs
+
+        count_pairs = 0
+        shape_pairs.clear()
+        line_canvas.delete("all")
+        generateShapes(3)
 
     generateShapes(3)
-
-
-
-    start_x = None
-    start_y = None
-
-
-    start_x = None
-
 
     frameShapes.place(relx=.5, rely=.65, anchor="center")
     frameNames.place(relx=.5, rely=.2, anchor="center")
